6092389802745e3bce818368a7d5add5e1c49ff0
Sowmya Kari <karisowmya70@gmail.com>
2023-07-25 14:39:11 -0400

    New changes

---
+++ index.js
@@ -0,0 +1,17 @@
+const containerE1 = document.querySelector(".container");
+const careers = ["developer","Web designer","Engineer"]
+let careerIndex = 0;
+let characterIndex = 0;
+updateText();
+function updateText(){
+  characterIndex++;
+  containerE1.innerHTML= `<h1> Iam a ${careers[careerIndex]}.slice(0,characterIndex)} </h1>`;
+  if(characterIndex === careers[careerIndex].length){
+     careerIndex++
+     characterIndex = 0;
+}
+  if(careerIndex === careers.length){
+    careerIndex = 0;
+  }
+setTimeout(updateText,400);
+}
